c972f17087c4a8251b517309f5124d3ac770d2bc
Cymon Luther Mantaring <mantaringcymonluther@gmail.com>
2026-03-26 22:51:30 +0800

    feat(auth): build sign-in and sign-up pages

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,4 +1,10 @@
+"use client";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
+      SignUp
+    </div>
+  );
 };
 export default SignUp;
