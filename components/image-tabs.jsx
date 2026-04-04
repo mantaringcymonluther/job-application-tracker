127d03d0851bebaba52734a1df6f574a7e437fe8
Cymon Luther Mantaring <mantaringcymonluther@gmail.com>
2026-04-04 19:30:08 +0800

    style: minor UI changes

--- components/image-tabs.jsx
+++ components/image-tabs.jsx
@@ -9,7 +9,7 @@ const ImageTabs = () => {
 
   return (
     // remove border-t
-    <section className="bg-white py-16">
+    <section className="bg-white py-16 border-t">
       <div className="container mx-auto px-4">
         <div className="mx-auto max-w-6xl">
           {/* Tabs */}
